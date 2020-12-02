4a59a86ba72ea3a69154dbd9494215c8095fba2c
Charles Norman <charlesrichardnorman@gmail.com>
2020-12-02 13:36:30 -0500

    Added tabs to gui

--- gui_layout.py
+++ gui_layout.py
@@ -1,38 +1,54 @@
 import PySimpleGUI as sg
-# region Layout
-layout = [[sg.Text('State:'), sg.Text(size=(20, 1), key='-STATE-')],
-          [sg.Text('Battery 6V:'), sg.Text(size=(20, 1), key='-BATTERY_6V-')],
-          [sg.Text('Cool:'), sg.Text(size=(20, 1), key='-COOL-')],
-          [sg.Text('Manual:'), sg.Text(
-              size=(20, 1), key='-MANUAL_SELECTED-')],
-          [sg.Text('On:'), sg.Text(size=(20, 1), key='-ON_SELECTED-')],
-          [sg.Text('Start:'), sg.Text(size=(20, 1), key='-START-')],
-          [sg.Text('Voltage Display:'), sg.Text(
-              size=(20, 1), key='-VOLTAGE_TEMP-')],
-          [sg.Text('Target Current:'), sg.Text(
-              size=(20, 1), key='-TARGET_CURRENT-')],
-          [sg.Text('Actual Current:'), sg.Text(
-              size=(20, 1), key='-ACTUAL_CURRENT-')],
-          [sg.Text('Target Voltage:'), sg.Text(
-              size=(20, 1), key='-TARGET_VOLTAGE-')],
-          [sg.Text('Actual Voltage:'), sg.Text(
-              size=(20, 1), key='-ACTUAL_VOLTAGE-')],
-          [sg.Text('Oven Temperature:'), sg.Text(
-              size=(20, 1), key='-OVEN_TEMP-')],
-          [sg.Text('Oven Set Point:'), sg.Text(
-              size=(20, 1), key='-OVEN_SET_POINT-')],
-          [sg.Text('Component Temperature:'), sg.Text(
-              size=(20, 1), key='-COMP_TEMPERATURE-')],
-          [sg.Radio('Display Volts', "RADIO_VOLT_TEMP", key='-DISPLAY_VOLTAGE-', enable_events=True), sg.Radio(
-              'Display Temp(C)', "RADIO_VOLT_TEMP", key='-DISPLAY_TEMPERATURE-', enable_events=True)],
-          [sg.R('6 Volt', "RADIO_6V_12V", key="-6VOLT-", enable_events=True),
-           sg.R('12 Volt', "RADIO_6V_12V", key="-12VOLT-", enable_events=True)],
-          [sg.R('ON ', "RADIO_ON_OFF", key="-ON-", enable_events=True),
-           sg.R('OFF', "RADIO_ON_OFF", key="-OFF-", enable_events=True)],
-          [sg.R('MAN ', "RADIO_MAN_AUTO", key="-MANUAL-", enable_events=True),
-           sg.R('AUTO', "RADIO_MAN_AUTO", key="-AUTO-", enable_events=True)]
-          ]
-window = sg.Window('F100B200 Tester', layout)
+
+
+# region Readings Tab
+readings_tab = [[sg.Text('State:'), sg.Text(size=(20, 1), key='-STATE-')],
+                [sg.Text('Battery 6V:'), sg.Text(
+                    size=(20, 1), key='-BATTERY_6V-')],
+                [sg.Text('Cool:'), sg.Text(size=(20, 1), key='-COOL-')],
+                [sg.Text('Manual:'), sg.Text(
+                    size=(20, 1), key='-MANUAL_SELECTED-')],
+                [sg.Text('On:'), sg.Text(size=(20, 1), key='-ON_SELECTED-')],
+                [sg.Text('Start:'), sg.Text(size=(20, 1), key='-START-')],
+                [sg.Text('Voltage Display:'), sg.Text(
+                    size=(20, 1), key='-VOLTAGE_TEMP-')],
+                [sg.Text('Target Current:'), sg.Text(
+                    size=(20, 1), key='-TARGET_CURRENT-')],
+                [sg.Text('Actual Current:'), sg.Text(
+                    size=(20, 1), key='-ACTUAL_CURRENT-')],
+                [sg.Text('Target Voltage:'), sg.Text(
+                    size=(20, 1), key='-TARGET_VOLTAGE-')],
+                [sg.Text('Actual Voltage:'), sg.Text(
+                    size=(20, 1), key='-ACTUAL_VOLTAGE-')],
+                [sg.Text('Oven Temperature:'), sg.Text(
+                    size=(20, 1), key='-OVEN_TEMP-')],
+                [sg.Text('Oven Set Point:'), sg.Text(
+                    size=(20, 1), key='-OVEN_SET_POINT-')],
+                [sg.Text('Component Temperature:'), sg.Text(
+                    size=(20, 1), key='-COMP_TEMPERATURE-')],
+                [sg.Radio('Display Volts', "RADIO_VOLT_TEMP", key='-DISPLAY_VOLTAGE-', enable_events=True), sg.Radio(
+                    'Display Temp(C)', "RADIO_VOLT_TEMP", key='-DISPLAY_TEMPERATURE-', enable_events=True)],
+                [sg.R('6 Volt', "RADIO_6V_12V", key="-6VOLT-", enable_events=True),
+                 sg.R('12 Volt', "RADIO_6V_12V", key="-12VOLT-", enable_events=True)],
+                [sg.R('ON ', "RADIO_ON_OFF", key="-ON-", enable_events=True),
+                 sg.R('OFF', "RADIO_ON_OFF", key="-OFF-", enable_events=True)],
+                [sg.R('MAN ', "RADIO_MAN_AUTO", key="-MANUAL-", enable_events=True),
+                 sg.R('AUTO', "RADIO_MAN_AUTO", key="-AUTO-", enable_events=True)]
+                ]
+# endregion
+# region Manual Tab
+manual_tab = [[sg.R('6 Volt', "RADIO_6V_12V", key="-6VOLT-", enable_events=True),
+               sg.R('12 Volt', "RADIO_6V_12V", key="-12VOLT-", enable_events=True)],
+              [sg.R('ON ', "RADIO_ON_OFF", key="-ON-", enable_events=True),
+               sg.R('OFF', "RADIO_ON_OFF", key="-OFF-", enable_events=True)],
+              [sg.R('MAN ', "RADIO_MAN_AUTO", key="-MANUAL-", enable_events=True),
+               sg.R('AUTO', "RADIO_MAN_AUTO", key="-AUTO-", enable_events=True)]
+              ]
+
+main_layout = [[sg.TabGroup([[
+    sg.Tab('Readings', readings_tab),
+    sg.Tab('Manual', manual_tab)
+]])]]
 # endregion
 # region Update GUI Values
 
@@ -118,4 +134,7 @@ def process_events(event, values, json_data):
     if event == "-OFF-":
         json_data['on_selected'] = 0
     return True
+
+
 # endregion
+window = sg.Window('F100B200 Tester', main_layout)
--- test.py
+++ test.py
@@ -1,20 +1,57 @@
-import warnings
-import serial
-import serial.tools.list_ports
+#!/usr/bin/env python
+import sys
+import PySimpleGUI as sg
+# import PySimpleGUIWeb as sg
 
-arduino_ports = [
-    p.device
-    for p in serial.tools.list_ports.comports()
-    # may need tweaking to match new arduinos
-    if ('Arduino' in p.description and not "Programming" in p.description)
-]
-if not arduino_ports:
-    raise IOError("No Arduino found")
-if len(arduino_ports) == 1:
-    arduino = arduino_ports[0]
+# Usage of Tabs in PSG
+#
+# sg.set_options(background_color='cornsilk4',
+#         element_background_color='cornsilk2',
+#         input_elements_background_color='cornsilk2')
 
-if len(arduino_ports) > 1:
-    warnings.warn('Multiple Arduinos found - using the first')
-    arduino = arduino_ports[0]
+sg.theme('Light Green 5')
 
-print(arduino)
+tab1_layout = [[sg.Text('This is inside tab 1', background_color='darkslateblue', text_color='white')],
+               [sg.Input(key='-in0-')]]
+
+tab2_layout = [[sg.Text('This is inside tab 2', background_color='tan1')],
+               [sg.Input(key='-in2-')]]
+
+
+tab3_layout = [[sg.Text('This is inside tab 3')],
+               [sg.Input(key='-in2-')]]
+
+tab4_layout = [[sg.Text('This is inside tab 4', background_color='darkseagreen')],
+               [sg.Input(key='-in3-')]]
+
+tab5_layout = [[sg.Text('This is inside tab 5')],
+               [sg.Input(key='-in4-')]]
+
+
+layout = [[sg.TabGroup([[sg.Tab('Tab 1', tab1_layout, background_color='darkslateblue', key='-mykey-'),
+                         sg.Tab('Tab 2', tab2_layout, background_color='tan1'),
+                         sg.Tab('Tab 3', tab3_layout)]],
+                       key='-group2-', title_color='red',
+                       selected_title_color='green', tab_location='right'),
+           sg.TabGroup([[sg.Tab('Tab 4', tab4_layout, background_color='darkseagreen', key='-mykey-'),
+                         sg.Tab('Tab 5', tab5_layout)]], key='-group1-', tab_location='top', selected_title_color='purple')],
+          # [sg.TabGroup([[sg.Tab('Tab 1', tab1_layout, background_color='darkslateblue', key='-mykey-'),
+          #                sg.Tab('Tab 2', tab2_layout, background_color='tan1'),
+          #                sg.Tab('Tab 3', tab3_layout)]],
+          #              key='-group3-', title_color='red',
+          #              selected_title_color='green', tab_location='left'),
+          #  sg.TabGroup([[sg.Tab('Tab 4', tab4_layout, background_color='darkseagreen', key='-mykey-'),
+          #                sg.Tab('Tab 5', tab5_layout)]], key='-group4-', tab_location='bottom', selected_title_color='purple')],
+          [sg.Button('Read')]]
+
+window = sg.Window('My window with tabs', layout,
+                   default_element_size=(12, 1))
+
+
+while True:
+    event, values = window.read()
+    sg.popup_non_blocking(event, values)
+    print(event, values)
+    if event == sg.WIN_CLOSED:           # always,  always give a way out!
+        break
+window.close()
